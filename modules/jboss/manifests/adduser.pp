0895169b5b7b0acb16a4743e8785f2dd64eca52c
Oscar Renalias <oscar.renalias@accenture.com>
2013-01-03 11:52:09 +0200

    Added support for creating a default admin user during the installation process

---
+++ modules/jboss/manifests/adduser.pp
@@ -0,0 +1,22 @@
+# Function that adds users to JBoss
+define jboss::adduser($name = $title, $password, $jbosspath) {
+	if($password == "") {
+		fail("Empty passwords are not allowed")
+	}
+
+	if($jbosspath == "") {
+		fail("Please provide the path to the JBoss base installation folder")
+	}
+
+	# check what type of user must be created and then run the correct command
+	case $type {
+		"management": { $command = "add-user.sh $name $password" }
+		"application": { $command = "add-user.sh -a $name $password" }
+		default: { fail("User type must either be 'management' or 'application'") }
+	}
+
+	exec { "adduser-$name":
+		command => $command,
+		path => [ "$jbosspath/bin" ]
+	}
+}
