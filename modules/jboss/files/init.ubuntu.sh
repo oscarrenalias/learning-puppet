8673d193fcfe182cd87ffdbe66b0b0260944b965
Oscar Renalias <oscar@renalias.net>
2013-01-03 14:00:02 +0000

    Added support for restarting the service

--- modules/jboss/files/init.ubuntu.sh
+++ modules/jboss/files/init.ubuntu.sh
@@ -17,22 +17,21 @@ export PATH=$JBOSS_HOME/bin:$PATH
 case "$1" in
     start)
         echo "Starting JBoss AS 7.1.1"
-        #original:
-        #sudo -u jboss sh ${JBOSS_HOME}/bin/standalone.sh
-
-        #updated:
         start-stop-daemon --start --quiet --background --chuid jboss --exec ${JBOSS_HOME}/bin/standalone.sh
     ;;
     stop)
         echo "Stopping JBoss AS 7.1.1"
-        #original:
-        #sudo -u jboss sh ${JBOSS_HOME}/bin/jboss-admin.sh --connect command=:shutdown
-
-        #updated:
         start-stop-daemon --start --quiet --chuid jboss --exec ${JBOSS_HOME}/bin/jboss-cli.sh -- --connect command=:shutdown
     ;;
+    restart)
+        echo "Stopping JBoss AS 7.1.1"
+        start-stop-daemon --start --quiet --chuid jboss --exec ${JBOSS_HOME}/bin/jboss-cli.sh -- --connect command=:shutdown
+
+        echo "Starting JBoss AS 7.1.1"
+        start-stop-daemon --start --quiet --background --chuid jboss --exec ${JBOSS_HOME}/bin/standalone.sh
+    ;;
     *)
-        echo "Usage: /etc/init.d/jboss {start|stop}"
+        echo "Usage: /etc/init.d/jboss {start|stop|restart}"
         exit 1
     ;;
 esac
