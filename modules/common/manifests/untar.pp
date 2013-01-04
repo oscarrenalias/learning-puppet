2a8b537a35536564744cdc9c876b98113e00f114
Oscar Renalias <oscar@renalias.net>
2013-01-04 10:08:50 +0000

    Some minor changes

--- modules/common/manifests/untar.pp
+++ modules/common/manifests/untar.pp
@@ -2,11 +2,12 @@ define common::untar($source = $title, $target, $ifNotExists) {
 	exec { "untar-$source-$target":
 		command => "tar zxvf $source -C $target",
 		path => [ "/bin", "/usr/bin", "/usr/local/bin" ],
-		require => File["$target"],
+		require => File["untar-target-$target"],
 		creates => $ifNotExists
 	}
 
-	file { "$target": 
+	file { "untar-target-$target":
+	        path => $target,
 		ensure => directory
 	}
 }
