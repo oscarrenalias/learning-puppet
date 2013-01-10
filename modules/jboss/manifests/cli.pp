7b5c699a117c439de310a33be1669a9393f5de46
Oscar Renalias <oscar@renalias.net>
2013-01-10 10:43:21 +0000

    Improved jboss::cli module so that it won't add system properties if they already exist

--- modules/jboss/manifests/cli.pp
+++ modules/jboss/manifests/cli.pp
@@ -2,6 +2,9 @@ define jboss::cli::sysproperty_add($user, $password, $name, $value, $jbossroot) 
        # run this command: 
        # jboss-cli.sh --user=admin --password=password --connect --command="/system-property=property:add(value=value)"
        exec { "jboss-cli-set-property-$name-$value":
-       	    command => "$jbossroot/bin/jboss-cli.sh --user=$user --password=$password --connect  --command=\/system-property=$name:add\(value=$value\)"
+       	    command => "$jbossroot/bin/jboss-cli.sh --user=$user --password=$password --connect  --command=\/system-property=$name:add\(value=$value\)",
+		# only run if the system property doesn't exist yet, to prevent multiple overriding executions
+		# TODO: make it so that callers of this defined resource can force override, and in that case we'd need a delete first and then an add
+	    onlyif => "$jbossroot/bin/jboss-cli.sh --user=$user --password=$password --connect --command=\/system-property=$name:read-resource|grep failed",
        }
 }
