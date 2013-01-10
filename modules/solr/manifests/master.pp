0420ea35db74b6e2fccdf3bb61b23daa5f106ad6
Oscar Renalias <oscar@renalias.net>
2013-01-10 10:45:37 +0000

    Added version-dependent configuration support, and added configuration for 3.6.2 as well as 4.0.0

--- modules/solr/manifests/master.pp
+++ modules/solr/manifests/master.pp
@@ -33,7 +33,7 @@ class solr::master($version, $config, $jbossroot) {
 
 	# put the Solr WAR in its right place
 	jboss::deploy { "jboss-deploy-solr-$version":
-		source => "/opt/solr/apache-solr-$version/dist/apache-solr-4.0.0.war",
+		source => "/opt/solr/apache-solr-$version/dist/apache-solr-$version.war",
 		target => "solr.war",
 		jbossroot => $jbossroot,
 		replace => true
@@ -41,6 +41,7 @@ class solr::master($version, $config, $jbossroot) {
 
 	solr::init_home { "solr-home":
 		solrhome => "/opt/solr/home",
+		version => $version,
 		config => $config,
 		replace => false,
 	}
@@ -49,12 +50,13 @@ class solr::master($version, $config, $jbossroot) {
 #
 # Initializes a Solr home with a default set of configuration files
 #
-define solr::init_home($solrhome, $config, replace = false) {
+define solr::init_home($solrhome, $version, $config, replace = false) {
 	# set up the Solr home if it doesn't exist
 	debug("solr::init_home: solrhome = $solrhome, config = $config, replace = $replace")
+
 	file { "solr-home-$solrhome-$config-$replace":
 	     path => $solrhome,
-	     source => "puppet:///modules/solr/configs/$config",
+	     source => "puppet:///modules/solr/configs/$version/$config",
 	     recurse => true,
 	     group => "jboss",
 	     owner => "jboss",
