913f9c95d902d65d544fa458cd2d97910235bc39
Oscar Renalias <oscar@renalias.net>
2013-01-10 14:37:45 +0000

    Do not create target folder by default

--- modules/common/manifests/unzip.pp
+++ modules/common/manifests/unzip.pp
@@ -7,12 +7,6 @@ define common::unzip($source = $title, $target, $ifNotExists) {
 	exec { "unzip-$source-$target":
 		command => "unzip $source -d $target",
 		path => [ "/bin", "/usr/bin", "/usr/local/bin" ],
-		require => File["unzip-target-$target"],
 		creates => $ifNotExists,
-	}
-
-	file { "unzip-target-$target":
-	        path => $target,
-		ensure => directory
 	}
 }
