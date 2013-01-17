35904c1a214e57e01afd8936411ab93e88bfc34e
Oscar Renalias <oscar@renalias.net>
2013-01-17 13:18:22 +0000

    Added retries and wait timeout, in case the server is still on its way up

--- modules/jboss/manifests/cli.pp
+++ modules/jboss/manifests/cli.pp
@@ -1,10 +1,15 @@
 define jboss::cli::sysproperty_add($user, $password, $name, $value, $jbossroot) {
        # run this command: 
        # jboss-cli.sh --user=admin --password=password --connect --command="/system-property=property:add(value=value)"
+  info("Adding JBoss system property: $name = $value")
+  
        exec { "jboss-cli-set-property-$name-$value":
        	    command => "$jbossroot/bin/jboss-cli.sh --user=$user --password=$password --connect  --command=\/system-property=$name:add\(value=$value\)",
 		# only run if the system property doesn't exist yet, to prevent multiple overriding executions
 		# TODO: make it so that callers of this defined resource can force override, and in that case we'd need a delete first and then an add
 	    onlyif => "$jbossroot/bin/jboss-cli.sh --user=$user --password=$password --connect --command=\/system-property=$name:read-resource|grep failed",
-       }
+            tries => 3,
+            timeout => 120,
+            try_sleep => 60,
+       }  
 }
