d3f5b84e34fc51a27176ff6e3a9586eed678294f
Oscar Renalias <oscar@renalias.net>
2013-01-17 22:46:11 +0000

    Made the code a little smarter even though we've had to resort to exec

--- modules/liferay/manifests/hotdeploy.pp
+++ modules/liferay/manifests/hotdeploy.pp
@@ -4,12 +4,24 @@
 define liferay::hotdeploy(
   $source = $title,
   $package,
-  $liferayhome
+  $liferayhome,
+  $jbosshome
 ) {
-  file { "liferay-hotdeploy-$source":
-    source => $source,
-    path => "$liferayhome/deploy/$package",
-    owner => "jboss",
-    group => "jboss",
+  #file { "liferay-hotdeploy-$source":
+  #  source => $source,
+  #  path => "$liferayhome/deploy/$package",
+  #  owner => "jboss",
+  #  group => "jboss",
+  #}
+  exec { "liferay-hotdeploy-$source":
+    command => "cp $source $liferayhome/deploy/$package",
+    path => [ "/bin", "/usr/bin" ],
+    creates => "$jbosshome/standalone/deployments/$package",
+    notify => Exec["fix-hotdeploy-permissions"],
+  }
+
+  exec { "fix-hotdeploy-permissions":
+    command => "chown jboss:jboss $liferayhome/deploy/$package",
+    path => ["/bin", "/usr/bin"],
   }
 }
