7c42b96facf7ee44d6a78d635615a4e9c3b8d9d7
Oscar Renalias <oscar@renalias.net>
2013-01-18 13:20:27 +0000

    liferay.solr.server moved to a standalone.xml

--- modules/liferay/manifests/solr.pp
+++ modules/liferay/manifests/solr.pp
@@ -3,8 +3,8 @@
 #
 class liferay::solr(
   $jbosshome = $liferay::params::jbosshome,
-  $version = $liferay::params::version,
-  $solrserver
+  $version = $liferay::params::version
+  #$solrserver
 ) inherits liferay::params {
 
   # TODO: we should probably not repeat this across manifests
@@ -21,16 +21,16 @@ class liferay::solr(
     source => "/tmp/solr-web-$version.war",
     package => "solr-web.war",
     liferayhome => "/opt/liferay",
-    require => Jboss::Cli::Sysproperty_add["liferay-solr-property"],
+    #require => Jboss::Cli::Sysproperty_add["liferay-solr-property"],
     jbosshome => $jbosshome,
   }
 
-  jboss::cli::sysproperty_add { "liferay-solr-property":
-    user => "admin",
-    password => "password",
-    name => "liferay.solr.server",
-    value => $solrserver,
-    jbossroot => $jbosshome,
-    timeout => 600,
-  }
+  #jboss::cli::sysproperty_add { "liferay-solr-property":
+  #  user => "admin",
+  #  password => "password",
+  #  name => "liferay.solr.server",
+  #  value => $solrserver,
+  #  jbossroot => $jbosshome,
+  #  timeout => 600,
+  #}
 }
